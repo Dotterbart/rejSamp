9882d85d2f8d5bdfb21a7219701e093e629cce81
Dotterbart <dominik.lenk@tu-dortmund.de>
2015-11-01 19:36:32 +0100

    test upgraded

--- tests/testthat/test_rejSamp.R
+++ tests/testthat/test_rejSamp.R
@@ -1,10 +1,17 @@
 context("rejSamp")
 
+funExp = function(fun, min, max) {
+  div = integrate(fun, min, max)$value
+  fun2 = function(x) x * fun(x) / div
+  return(integrate(fun2, min, max)$value)
+}
 fun1 = function(x) x
 fun2 = function(x) dunif(x, 1, 5)
 fun3 = function() runif(1, 1, 5)
 fun4 = function() runif(7, 1, 5)
 xxx <- "Hello"
+
+set.seed(9247)
 
 test_that("rejSamp", {
   expect_true(is.numeric(rejSamp(fun1, 10, -5000, 5000)))
@@ -18,4 +25,6 @@ test_that("rejSamp", {
   expect_error(rejsamp(fun1, 10, 1, 5, xxx, fun3, 20))
   expect_error(rejsamp(fun1, 10, 1, 5, fun2, xxx, 20))
   expect_error(rejsamp(fun1, 10, 1, 5, fun2, fun4, 20))
+  expect_that(mean(rejSamp(fun1, 10000, 1, 5)), is_less_than(1.03 * funExp(fun1, 1, 5)))
+  expect_that(mean(rejSamp(fun1, 10000, 1, 5)), is_more_than(0.97 * funExp(fun1, 1, 5)))
 })
