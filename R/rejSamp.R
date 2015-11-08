67b9822544943fc92df77d12013e43ffe73a941c
Dotterbart <dominik.lenk@tu-dortmund.de>
2015-11-08 13:01:03 +0100

    mw II

--- R/rejSamp.R
+++ R/rejSamp.R
@@ -1,26 +1,26 @@
 #' Generate random numbers by rejection sampling
 #'
-#' @param f [\code{function}]
+#' @param f [\code{function}]\cr
 #'  Function to be interpreted as probability density function.
-#' @param n  [\code{integer(1)}]
+#' @param n  [\code{integer(1)}]\cr
 #'  Amount of random numbers to be generated. Default is 1.
-#' @param min [\code{numeric(1)}]
-#' @param max [\code{numeric(1)}]
+#' @param min [\code{numeric(1)}]\cr
+#' @param max [\code{numeric(1)}]\cr
 #'  Interval the generated random numbers are from. Default is (0, 1).
-#' @param g [\code{function}]
+#' @param g [\code{function}]\cr
 #'  Probability density function of the instrumental distribution.
-#' @param rg  [\code{function}]
+#' @param rg  [\code{function}]\cr
 #'  Function that generates random numbers from the instrumental distribution.
-#' @param g.factor [\code{numeric(1)}]
+#' @param g.factor [\code{numeric(1)}]\cr
 #'  Factor for \code{g}. Default is 1.
 #' @return
-#'  Random numbers from the function \code{f} interpreted as probality density function on the interval (\code{min}, \code{max}).
+#'  \code{n} Random numbers from the function \code{f} interpreted as probality density function on the interval (\code{min}, \code{max}).
 #' @details
 #' If no instrumental distribution is specified, the uniform distribution is used.
 #'
-#' The call \code{g}(x) should produce the probability of the realisation x for the instrumental distribution \code{g}.
+#' The call \code{g}(x) should return the probability of the realisation x for the instrumental distribution \code{g}.
 #'
-#' The choice of \code{g.factor} should guarantee \code{f(x) < g.factor * g(x)}.
+#' The choice of \code{g.factor} should guarantee \code{f}(x) < \code{g.factor} * \code{g}(x).
 #' @examples
 #' test <- function(x) x^2
 #' rejSamp(f = test, n = 10, min = -1, max = 1)
